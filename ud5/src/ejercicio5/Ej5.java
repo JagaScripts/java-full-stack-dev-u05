975b8561a63118605fce16e5b3903fdda90406f2
Jose A González <jagascripts@gmail.com>
2022-04-09 09:56:23 +0200

    Ejercico 5 comprobada casuistica para que el ususario no se puedan generar errores por parte del usuario.

--- ud5/src/ejercicio5/Ej5.java
+++ ud5/src/ejercicio5/Ej5.java
@@ -15,29 +15,58 @@ public class Ej5 {
 		/*Lee un número por teclado e indica
 		  si es divisible entre 2 (resto = 0).
 		  Si no lo es , también debemos indicarlo*/
-		
-		String texto;
+
+		String entradaTeclado;
 		int numero;
 		int resto;
-		
-		//Pedimos número
-		texto = JOptionPane.showInputDialog("Introduce el radio");
-		
-		//Parseamos a número
-		numero = Integer.parseInt(texto);
-		
-		//Obtenemos el resto con el modulo
-		resto = numero % 2;
-		
-		//Comprobamos si es dvisible entre 2 resto = 0
-		if (resto == 0) {
-			//Mostramos que si que es divisible por 2
-			JOptionPane.showMessageDialog(null, "El número" + numero + " SI es divisible ente 2");
-		} else {
-			//Mostramos que no que es divisible por 2
-			JOptionPane.showMessageDialog(null, "El número" + numero + " NO es divisible ente 2");
-		}
-		
+		boolean esNumerico;
+
+		do {
+
+			//Pedimos número
+			entradaTeclado = JOptionPane.showInputDialog("Introduce el radio");
+
+			if (entradaTeclado != null) {
+
+				//Como aún no hemos visto manejo de excepciones
+				//Compruebo mediante el metodo matches del tipo String si lo introducido es un número
+				esNumerico = entradaTeclado.matches("-?\\d+");
+
+				if (esNumerico) {
+
+					//Parseamos a número
+					numero = Integer.parseInt(entradaTeclado);
+
+					//Obtenemos el resto con el modulo
+					resto = numero % 2;
+
+					//Comprobamos si es dvisible entre 2 resto = 0
+					if (resto == 0) {
+						
+						//Mostramos que si que es divisible por 2
+						JOptionPane.showMessageDialog(null, "El número" + numero + " SI es divisible ente 2");
+						
+					} else {
+						
+						//Mostramos que no que es divisible por 2
+						JOptionPane.showMessageDialog(null, "El número" + numero + " NO es divisible ente 2");
+					}
+					
+				} else {
+					
+					JOptionPane.showMessageDialog(null, "No has introducido un número valido");
+					
+				}
+				
+			} else {
+
+				esNumerico = false;
+
+			}
+
+		} while (!esNumerico && entradaTeclado != null);
+
+		JOptionPane.showMessageDialog(null, "La plicación se cerrara");		
 
 	}
 
